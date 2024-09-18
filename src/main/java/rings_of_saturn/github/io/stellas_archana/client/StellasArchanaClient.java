1731264dcceaae6b7d17c111552e388023451fa8
TLS <thecolorizedsystem@gmail.com>
2024-09-18 22:11:01 +0300

    ⚧️Changed Spell Book Model to be 1 handed!

--- src/main/java/rings_of_saturn/github/io/stellas_archana/client/StellasArchanaClient.java
+++ src/main/java/rings_of_saturn/github/io/stellas_archana/client/StellasArchanaClient.java
@@ -1,8 +1,6 @@
 package rings_of_saturn.github.io.stellas_archana.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class StellasArchanaClient implements ClientModInitializer {
 
