02dc11e70eb54d9d618d6bf9b13c9fbb301ee873
TLS <thecolorizedsystem@gmail.com>
2024-09-18 17:19:23 +0300

    ⭐ Initial Commit!

---
+++ src/main/java/rings_of_saturn/github/io/stellas_archana/StellasArchana.java
@@ -0,0 +1,10 @@
+package rings_of_saturn.github.io.stellas_archana;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StellasArchana implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/rings_of_saturn/github/io/stellas_archana/client/StellasArchanaClient.java
@@ -0,0 +1,10 @@
+package rings_of_saturn.github.io.stellas_archana.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StellasArchanaClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
